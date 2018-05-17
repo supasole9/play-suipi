564ba3958cb34ba05cea9f6fb1858f920b9536f8
Ross Salanoa <rosssalanoa@Rosss-MacBook-Pro.local>
2018-05-17 16:17:13 -0600

    added Vue Router

--- src/main.js
+++ src/main.js
@@ -1,7 +1,22 @@
 import Vue from 'vue';
+import VueRouter from 'vue-router';
 import App from './App.vue';
+import Dashboard from './Dashboard.vue';
+import Home from './Home.vue';
+
+Vue.use(VueRouter);
+
+const routes = [
+  { path: '/dashboard', component: Dashboard },
+  { path: '/', component: Home }
+];
+
+const router = new VueRouter({
+  routes
+});
 
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
